3bb78805112361ddaca1cc153c1fbb851b5e0c25
Amanda Oliveira <100137341+amandazzoc@users.noreply.github.com>
2024-07-16 17:55:05 -0300

    Add files via upload

---
+++ src/main/java/org/example/Produto.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface Produto {
+
+    double getValor(); // Todo método sem corpo de uma interfáce é abstrata, dessa forma não é necessário colocar "abstract" e nem "public" pois seus métodos já são públicos por padrão
+
+}
